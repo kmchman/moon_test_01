c465b1c6c1e04031996cd5390ad144bd4ad5a0f7
david <david@singtagames.com>
2018-06-05 19:44:06 +0900

    Minor

--- Assets/02_Scripts/Data/_Datatable.cs
+++ Assets/02_Scripts/Data/_Datatable.cs
@@ -3,5 +3,34 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 public class _Datatable {
-	
+	public class CharData {
+		public int ID;
+		public int JobTitle;
+		public string Race;
+		public string Role;
+		public string Attack_RangeType;
+		public int MoveSpeed;
+		public int ReservedPos;
+		public int Protect;
+		public int Scale;
+		public int EffectScale;
+		public int UIScale;
+		public int DialogScale;
+		public float DialogOffsetX;
+		public int DialogOffsetY;
+		public int PrefabName;
+		public string FaceImage;
+	};
+	public Dictionary<int, CharData> dtCharData = new Dictionary<int, CharData>();
+	public void LoadCharDatas(IDictionary dic) {
+		var enumerator = dic.GetEnumerator();
+		while (enumerator.MoveNext()) {
+			LoadCharData(enumerator.Key, enumerator.Value);
+		}
+	}
+	public void LoadCharData(object key, object value) {
+		IDictionary v = (IDictionary)value;
+
+		CharData i = new CharData();
+	}
 }
--- Assets/02_Scripts/Tools/MCodeGenerator.cs
+++ Assets/02_Scripts/Tools/MCodeGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 public class MCodeGenerator {
 	
@@ -37,9 +38,74 @@ public class MCodeGenerator {
 		sb.Append("using System.Collections;\n");
 		sb.Append("using System.Collections.Generic;\n");
 		sb.Append("public class _Datatable {\n");
-		sb.Append("}");
 
+		GenTableCode(sb, "CharData", dic);
+		sb.Append("}");
 		CreateDtFile(DATATABLE_DEF_PATH, sb.ToString());
+
 		Debug.Log("Complete GenDatatableCode");
+	}
+
+
+	static void GenTableCode(StringBuilder sb, string dtName, IDictionary dic) {
+
+		string keyType = GetKeyType(dic);	
+
+//		string keyType = (string)types[keyName];
+		sb.AppendFormat("\tpublic class {0} {{\n", dtName);
+		var enumerator = dic.GetEnumerator();
+		while (enumerator.MoveNext()) 
+		{
+			GenVariableCode(sb, (string)enumerator.Key, (string)enumerator.Value); 
+		}
+		sb.AppendFormat("\t}};\n");
+
+		sb.AppendFormat("\tpublic Dictionary<{1}, {0}> dt{0} = new Dictionary<{1}, {0}>();\n", dtName, keyType);
+
+		sb.AppendFormat("\tpublic void Load{0}s(IDictionary dic) {{\n", dtName);
+		sb.Append("\t\tvar enumerator = dic.GetEnumerator();\n");
+		sb.Append("\t\twhile (enumerator.MoveNext()) {\n");
+		sb.AppendFormat("\t\t\tLoad{0}(enumerator.Key, enumerator.Value);\n", dtName);
+		sb.Append("\t\t}\n");
+		sb.Append("\t}\n");
+
+		sb.AppendFormat("\tpublic void Load{0}(object key, object value) {{\n", dtName);
+//		sb.AppendFormat("\t\t{0} k;\n", keyType);
+		sb.AppendFormat("\t\tIDictionary v = (IDictionary)value;\n\n");
+		sb.AppendFormat("\t\t{0} i = new {0}();\n", dtName);
+//		sb.AppendFormat("\t\tdt{0}[k] = i;\n", dtName);
+		sb.Append("\t}\n");
+	}
+
+	static void GenVariableCode(StringBuilder sb, string name, string type) 
+	{
+		if (type.Contains("*")) {
+			type = type.Replace("*", string.Empty);
+		}
+
+		if (type.Equals("mtext")) {
+//			string mlName = name.Replace('_', '.');
+//			sb.AppendFormat("\t\tpublic virtual string {0} {{ get {{ return Datatable.Inst.GetML(GetType(), \"{1}\", {2}.ToString()); }} }}\n", name, mlName, key);
+			sb.AppendFormat("\t\tpublic string {0};\n", name);
+		} 
+		else if(type.Equals("String"))
+		{
+			sb.AppendFormat("\t\tpublic string {0};\n", name);
+		}
+		else { 
+			sb.AppendFormat("\t\tpublic {0} {1};\n", type, name);
+		}
+	}
+
+	static string GetKeyType(IDictionary dic)
+	{
+		var enumerator = dic.GetEnumerator();
+		while(enumerator.MoveNext())
+		{
+			if (enumerator.Value.ToString().Contains("*"))
+				return enumerator.Value.ToString().Replace("*", string.Empty);
+		}
+		Debug.LogError("key not found");
+		return string.Empty;
 	}
 }
