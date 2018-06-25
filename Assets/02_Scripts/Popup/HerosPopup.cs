a31ce6c7f3d63e6a38a732538568957f8fe0230c
david <david@singtagames.com>
2018-06-25 19:23:38 +0900

    히어로 팝업 수정

--- Assets/02_Scripts/Popup/HerosPopup.cs
+++ Assets/02_Scripts/Popup/HerosPopup.cs
@@ -7,7 +7,7 @@ public class HerosPopup : MoonPopupUI {
 
 	[SerializeField] private HeroListItem		m_HeroItemPrefab;
 	[SerializeField] private Transform 			m_HeroListParent;
-	[SerializeField] private ScrollRect			m_ScrollRect;
+	[SerializeField] private STScrollRect		m_ScrollRect;
 
 	public override void Show(params object[] values)
 	{
@@ -16,13 +16,14 @@ public class HerosPopup : MoonPopupUI {
 		var enumerator = Datatable.Inst.dtHeroData.GetEnumerator();
 		while (enumerator.MoveNext()) {
 			Debug.Log("hero : " + enumerator.Current.Value.Name);
-//			Instantiate
+			m_ScrollRect.GetReuseItem(m_HeroItemPrefab, null);
 		}
 		CompleteShowAnimation();
 	}
 
 	public override void Hide(params object[] values)
 	{
+		m_ScrollRect.Clear();
 		for (int i = 0; i < 5; i++) 
 		{
 
