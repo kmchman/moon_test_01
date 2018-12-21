63bf2f0fe04f1e7016e7656c32e3898b614c8543
david <david@singtagames.com>
2018-12-21 17:46:48 +0900

    Title, Lobby 텍스쳐 변경

---
+++ Assets/02_Scripts/Global/SafeAreaHelper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+[RequireComponent(typeof(RectTransform))]
+[ExecuteInEditMode]
+public class SafeAreaHelper : MonoBehaviour
+{
+	private void Start()
+	{
+		ApplySafeArea();
+	}
+
+	private void ApplySafeArea()
+	{
+		float div_width = 1f / Screen.width, div_height = 1f / Screen.height;
+		Rect safeArea = GetSafeArea();
+		Vector2 anchorMin = safeArea.position;
+		Vector2 anchorMax = safeArea.position + safeArea.size;
+
+		anchorMin.x *= div_width;
+		anchorMin.y *= div_height;
+		anchorMax.x *= div_width;
+		anchorMax.y *= div_height;
+
+		RectTransform rectTransform = GetComponent<RectTransform>();
+		rectTransform.anchorMin = anchorMin;
+		rectTransform.anchorMax = anchorMax;
+	}
+
+	private Rect GetSafeArea()
+	{
+#if UNITY_EDITOR && IPHONE_X_TEST
+		return new Rect(132f, 63f, 2172f, 1062f);
+#else
+		return Screen.safeArea;
+#endif
+	}
+}
--- Assets/Editor/MMenu.cs
+++ Assets/Editor/MMenu.cs
@@ -8,11 +8,8 @@ public class MMenu {
 	[MenuItem("MProject/Generate Data/Table", false, 0)]
 	static public void GenerateTable()
 	{
-<<<<<<< Updated upstream:Assets/Editor/MMenu.cs
 		MCodeGenerator.GenerateDatatable();
-=======
-		MCodeGenerator.GenDatatable ();
->>>>>>> Stashed changes:Assets/02_Scripts/Tools/MMenu.cs
+//		MCodeGenerator.GenDatatable ();
 	}
 
 }
