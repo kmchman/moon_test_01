c2773c672b9995f99852a92f040b1694bb622bd8
com-PC\com <david>
2019-07-05 00:05:59 +0900

    에셋번들 테스트

---
+++ Assets/00_Test/AssetBundle/Editor/BundleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/ Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(@"C:\dev\assetBundle", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+    }
+
+}
---
+++ Assets/00_Test/AssetBundle/LoadAssetBundles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+    AssetBundle myLoadedAssetbundles;
+    public string path;
+    public string testAssetName;
+
+    void Start()
+    {
+        LoadAssetBundle(path);
+        InstantiateObjectFromBundle(testAssetName);
+    }
+
+    void LoadAssetBundle(string bundleUrl)
+    {
+        myLoadedAssetbundles = AssetBundle.LoadFromFile(bundleUrl);
+        Debug.Log(myLoadedAssetbundles == null ? "failed to load assetBundles" : "AssetBundle succesfully loaded");
+
+    }
+
+    void InstantiateObjectFromBundle(string assetName)
+    {
+        var prefab = myLoadedAssetbundles.LoadAsset(assetName);
+        Instantiate(prefab);
+    }
+}
