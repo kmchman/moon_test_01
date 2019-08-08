c65f83345cb4051eb166805d99c9825f7a338f48
kmchman <kmchman@naver.com>
2019-08-08 14:43:09 +0900

    Test02 팝업 수정

--- Assets/02_Scripts/Popup/TestPopup02.cs
+++ Assets/02_Scripts/Popup/TestPopup02.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class TestPopup02 : MoonPopupUI {
-    
+
+    [SerializeField] private STScrollRect m_ScrollRect;
+
     protected override void Open(params object[] values)
     {
         var enumertor = Table.tb_Building_Base.map.GetEnumerator();
