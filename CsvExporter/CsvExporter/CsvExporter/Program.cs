48d4e243e84c9639235b6fa9fe80e29f16d2c303
david0305 <kmchman@gmail.com>
2021-01-20 18:13:38 +0900

    csvExport 추가

---
+++ CsvExporter/CsvExporter/CsvExporter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace CsvExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application application = new Application();
+            Workbook workBook = application.Workbooks.Open(Filename: @"D:\etg2_product.xlsx");   
+            Worksheet workSheet = workBook.Worksheets.Item[1];
+
+            Range range = workSheet.Range["A3", Type.Missing];
+            range.EntireRow.Delete();
+            //workBook.SaveAs(@"D:\etg2_product.csv", XlFileFormat.xlCSVWindows);
+
+
+            workBook.SaveAs(@"D:\etg2_product.csv", FileFormat:Microsoft.Office.Interop.Excel.XlFileFormat.xlCSV);
+
+            //workBook.SaveAs(Filename: @"D:\etg2_product.csv", FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlCSVWindows);
+            workBook.Close(false, Type.Missing, Type.Missing);
+            application.Quit();
+            Console.WriteLine("Save");
+            Console.ReadLine();
+        }
+    }
+}
